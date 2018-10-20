c42acf307edcc38d3d826ae6f69c6898d834dcf6
ayaleneh <getaunayaleneh@gmail.com>
2018-10-20 13:23:23 -0400

    i add some modification to ma javascript and html file

--- main.js
+++ main.js
@@ -2,3 +2,18 @@ let age;
 age=26;
 age=26+1;
 console.log("age is currently "+ age);
+console.log("hello world");
+let string =true;
+
+if (string)
+{
+    console.log("it is currently true");
+}
+document.writeln("hello there ");
+let pro= prompt("enter your favorite color that you want");
+if (pro=="abebe"){
+    alert("wellcome abebe");
+}
+alert("let me give you achoice");
+prompt("choice your preferred subject or feild?");
+confirm("you sure you want this subject");
